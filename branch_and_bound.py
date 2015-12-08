d832f57ddbd57de36f2a7eda8548f29cd9d93d81
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2015-12-07 21:16:04 -0500

    Arrived at the correct answer but need much cleanup

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -6,6 +6,10 @@ items = {1:{'value':727,'weight':32},2:{'value':763,'weight':40},3:{'value':60,'
          7:{'value':414,'weight':3},8:{'value':880,'weight':13},9:{'value':133,'weight':6},
          10:{'value':820,'weight':39}}
 
+def n(index):
+    node = nodes[index]
+    print('id:{id} w:{weight} b:{bound} parent:{parent} children:{children}'.format(id=node.id,weight=node.weight,bound=node.bound,children=node.child_ids,parent=node.parent_id))
+
 for item_id,item in items.items():
     items[item_id]['value-ratio'] = item['value']/item['weight']
 
@@ -17,16 +21,29 @@ nodes[len(nodes)]=origin
 max_value = max(origin.value,0)
 node = origin
 while node.bound != node.value:
-    if len(node.child_ids) > 0:
-        if nodes[node.child_ids[0]].bound <= max_value and nodes[node.child_ids[1]].bound <= max_value:
+    n(node.id)
+    if len(node.child_ids) > 0: 
+        if (nodes[node.child_ids[0]].bound <= max_value or nodes[node.child_ids[0]].weight > constraints['weight']) and \
+            (nodes[node.child_ids[1]].bound <= max_value or nodes[node.child_ids[1]].weight > constraints['weight']):
             #nodes[node.parent_id].bound = max(nodes[node.child_ids[0]].bound,nodes[node.child_ids[1]].bound)
-            nodes[node.id].bound = max(nodes[node.child_ids[0]].bound,nodes[node.child_ids[1]].bound)
+            possible_bounds = []
+            if nodes[node.child_ids[1]].weight <= constraints['weight']:
+                possible_bounds.append(nodes[node.child_ids[1]].bound)
+            if nodes[node.child_ids[0]].weight <= constraints['weight']:
+                possible_bounds.append(nodes[node.child_ids[0]].bound)
+            nodes[node.id].bound = max(possible_bounds)
             node = nodes[node.parent_id]
             continue
         elif nodes[node.child_ids[0]].bound > max_value and nodes[node.child_ids[0]].weight < constraints['weight']:
             node = nodes[node.child_ids[0]]
+            continue
         elif nodes[node.child_ids[1]].bound > max_value and nodes[node.child_ids[1]].weight < constraints['weight']:
             node = nodes[node.child_ids[1]]
+            continue
+        else:
+            nodes[node.id].bound = max(nodes[node.child_ids[0]].bound,nodes[node.child_ids[1]].bound)
+            node = nodes[node.parent_id]
+            continue
     included_item_ids = node.included_item_ids
     excluded_item_ids = node.excluded_item_ids
     remaining_items_sorted = sort_items_by_efficiency(items,included_item_ids,excluded_item_ids) 
@@ -70,7 +87,10 @@ while node.bound != node.value:
             max_value = max(better_node.value,max_value)
             print("Exclude: "+str(most_efficient_item['item_id'])+" profit "+str(excluded_item_node.value)+" weight "+str(excluded_item_node.weight)+" Include: profit "+str(included_item_node.value)+" weight "+str(included_item_node.weight))
         else:
-            nodes[node.id].bound = node.value
+            nodes[node.id].bound = node.value # 
+            if node.id == 15:
+                print(15)
+            nodes[included_item_node.id].bound = included_item_node.value
             node = nodes[node.parent_id]
             continue
     elif excluded_item_node.weight > constraints['weight']:
@@ -82,4 +102,8 @@ while node.bound != node.value:
         node = better_node
     if node.parent_id is None:
         break
+best_node = None
+for node_id, node in nodes.items():
+    if node.value == max_value:
+        print(node_id)
 print('test')

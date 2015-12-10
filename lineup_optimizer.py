239f393e1f8638ecd82d1d3921778f70c81e9d48
Spicysheep <rjzotti@gmail.com>
2015-12-10 16:48:29 -0500

    Reverted

--- lineup_optimizer.py
+++ lineup_optimizer.py
@@ -67,7 +67,7 @@ while origin.bound is not max_value:
         included_item_node = Node(own_id=node_count,parent_id=node.id,all_items=items,
                                   included_item_ids=candidate_included_items,
                                   excluded_item_ids=excluded_item_ids,
-                                  constraints=constraints)
+                                  constraints=constraints,generic_constraint='salary')
         nodes[included_item_node.id]=included_item_node
         candidate_excluded_items = excluded_item_ids[:]
         candidate_excluded_items.append(most_efficient_item['item_id'])
@@ -75,33 +75,32 @@ while origin.bound is not max_value:
         excluded_item_node = Node(own_id=node_count,parent_id=node.id,all_items=items,
                                   included_item_ids=included_item_ids,
                                   excluded_item_ids=candidate_excluded_items,
-                                  constraints=constraints)
+                                  constraints=constraints,generic_constraint='salary')
         nodes[excluded_item_node.id]=excluded_item_node
         nodes[node.id].set_child_ids([included_item_node.id,excluded_item_node.id])
         better_node = None
-        for constraint_name, constraint_value in constraints.items():
-            if included_item_node.weights[constraint_name] <= constraint_value and excluded_item_node.weights[constraint_name] <= constraint_value:
-                if included_item_node.bound > excluded_item_node.bound:
-                    if included_item_node.bound > max_value:
-                        better_node = included_item_node
-                        worse_node = excluded_item_node
-                        max_value = max(better_node.value,max_value)
-                        #print("Include: "+str(most_efficient_item['item_id'])+": profit "+str(included_item_node.value)+" weight "+str(included_item_node.weight)+" Exclude: profit "+str(excluded_item_node.value)+" weight "+str(excluded_item_node.weight))
-                    else:
-                        node = nodes[node.parent_id]
-                        continue
-                else:
-                    better_node = excluded_item_node
-                    worse_node = included_item_node
+        if all(included_item_node.weights[constraint_name] <= constraint_value for constraint_name,constraint_value in constraints.items()) and all(excluded_item_node.weights[constraint_name] <= constraint_value for constraint_name,constraint_value in constraints.items()):
+            if included_item_node.bound > excluded_item_node.bound:
+                if included_item_node.bound > max_value:
+                    better_node = included_item_node
+                    worse_node = excluded_item_node
                     max_value = max(better_node.value,max_value)
-                    #print("Exclude: "+str(most_efficient_item['item_id'])+" profit "+str(excluded_item_node.value)+" weight "+str(excluded_item_node.weight)+" Include: profit "+str(included_item_node.value)+" weight "+str(included_item_node.weight))
-            elif included_item_node.weights[constraint_name] > constraint_value:
-                nodes[node.id].bound = node.value # 
-                nodes[included_item_node.id].bound = included_item_node.value
-                node = nodes[node.parent_id]
-                continue
-            if better_node is not None:
-                node = better_node
+                    #print("Include: "+str(most_efficient_item['item_id'])+": profit "+str(included_item_node.value)+" weight "+str(included_item_node.weights['weight'])+" Exclude: profit "+str(excluded_item_node.value)+" weight "+str(excluded_item_node.weights['weight']))
+                else:
+                    node = nodes[node.parent_id]
+                    continue
+            else:
+                better_node = excluded_item_node
+                worse_node = included_item_node
+                max_value = max(better_node.value,max_value)
+                #print("Exclude: "+str(most_efficient_item['item_id'])+" profit "+str(excluded_item_node.value)+" weight "+str(excluded_item_node.weights['weight'])+" bound: "+str(excluded_item_node.bound)+" Include: profit "+str(included_item_node.value)+" weight "+str(included_item_node.weights['weight'])+" bound: "+str(included_item_node.bound))
+        elif any(included_item_node.weights[constraint_name] > constraint_value for constraint_name,constraint_value in constraints.items()):
+            nodes[node.id].bound = node.value # 
+            nodes[included_item_node.id].bound = included_item_node.value
+            node = nodes[node.parent_id]
+            continue
+        if better_node is not None:
+            node = better_node
 print('Best node:')
 for node_id, node in nodes.items():
     if node.value == max_value and len(node.child_ids) > 0:
